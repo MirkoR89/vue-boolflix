5d82c9dfdfae3f483b4318587902e2326ba20289
Mirko <mirko.rinelli@gmail.com>
2021-01-12 15:44:00 +0100

    Working on API TMDB

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,16 @@
 new Vue ({
   el: '#root',
   data: {
-    movieList: []
+    search: null,
+    moviesList: null
   },
-  mounted(){
-    axios
-    .get('')
+  methods: {
+    sendRequest(){
+      axios
+      .get(`https://api.themoviedb.org/3/search/movie?api_key=9ef5a9e86131aa08d5c204c4cea437a0&query= ${this.search}`)
+      .then(response => {
+        console.log(response.data.response);
+      })
+    }
   }
 })
