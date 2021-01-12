51b9dc030df2a4c051b1609bbd094efc72f070e0
Mirko <mirko.rinelli@gmail.com>
2021-01-12 16:14:53 +0100

    Millestone 1 - Complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,16 @@ new Vue ({
   el: '#root',
   data: {
     search: null,
-    moviesList: null
+    moviesList: null,
+    personalApiKey: '9ef5a9e86131aa08d5c204c4cea437a0'
   },
   methods: {
     sendRequest(){
       axios
-      .get(`https://api.themoviedb.org/3/search/movie?api_key=9ef5a9e86131aa08d5c204c4cea437a0&query= ${this.search}`)
+      .get(`https://api.themoviedb.org/3/search/movie?api_key=${this.personalApiKey}&query= ${this.search}`)
       .then(response => {
-        console.log(response.data.response);
+        console.log(response.data.results);
+        this.moviesList = response.data.results
       })
     }
   }
