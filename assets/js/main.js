4e65171f851dba9bb44431fb9e068f34c73f0dc9
Mirko <mirko.rinelli@gmail.com>
2021-01-12 17:34:03 +0100

    Selection dinamic vote_average

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,23 @@
-new Vue ({
+new Vue({
   el: '#root',
   data: {
+    personalApiKey: '9ef5a9e86131aa08d5c204c4cea437a0',
     search: null,
     moviesList: null,
-    personalApiKey: '9ef5a9e86131aa08d5c204c4cea437a0'
+    votePosition: null
   },
   methods: {
-    sendRequest(){
+    // Function to call back API Data
+    sendRequest() {
+      this.votePosition = index
       axios
-      .get(`https://api.themoviedb.org/3/search/movie?api_key=${this.personalApiKey}&query= ${this.search}`)
-      .then(response => {
-        console.log(response.data.results);
-        this.moviesList = response.data.results
-      })
+        .get(`https://api.themoviedb.org/3/search/movie?api_key=${this.personalApiKey}&query= ${this.search}`)
+        .then(response => {
+          //console.log(response.data.results);
+          this.moviesList = response.data.results
+          console.log(this.moviesList[this.votePosition].vote_average);
+        }
+      )
     }
   }
 })
