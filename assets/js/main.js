04c0edc4cc108a3648e0d3ce8681be0406dbe3eb
Mirko <mirko.rinelli@gmail.com>
2021-01-12 18:09:54 +0100

    Vote to stars

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,18 +4,21 @@ new Vue({
     personalApiKey: '9ef5a9e86131aa08d5c204c4cea437a0',
     search: null,
     moviesList: null,
-    votePosition: null
   },
   methods: {
     // Function to call back API Data
     sendRequest() {
-      this.votePosition = index
       axios
         .get(`https://api.themoviedb.org/3/search/movie?api_key=${this.personalApiKey}&query= ${this.search}`)
         .then(response => {
-          //console.log(response.data.results);
           this.moviesList = response.data.results
-          console.log(this.moviesList[this.votePosition].vote_average);
+
+          //Loop to move vote to stars
+          this.moviesList.forEach(item => {
+              let starsVote = Number(Math.floor(item.vote_average / 2));
+              return item.vote = starsVote
+          });
+
         }
       )
     }
